d0ab03a8f8807c51adb15c57a244527c1b448cd2
tiago.blima <tiago.blima02@gmail.com>
2021-06-01 21:13:52 -0300

    infinity process

--- main.py
+++ main.py
@@ -68,12 +68,13 @@ elif args.mode == 2:
         counter += 1
 elif args.mode == 3:
 
-    vi_mem = VirtualMemory(MAXIMUM_PHYSICAL_MEMORY,MAXIMUM_VIRTUAL_MEMORY, PAGE_SIZE)
-    for process_ in process_list:
-        vi_mem.allocate(process_)
-    print("Loading process from the memory", end='')
-    print("-"*40)
-    for process_ in process_list:
-        print(f'Loading the process {process_.name} from the Memory')
-        vi_mem.get_process(process_.id)
-    vi_mem.memory_status()
+    while True:
+        vi_mem = VirtualMemory(MAXIMUM_PHYSICAL_MEMORY,MAXIMUM_VIRTUAL_MEMORY, PAGE_SIZE)
+        for process_ in process_list:
+            vi_mem.allocate(process_)
+        print("Loading process from the memory", end='')
+        print("-"*40)
+        for process_ in process_list:
+            print(f'Loading the process {process_.name} from the Memory')
+            vi_mem.get_process(process_.id)
+        vi_mem.memory_status()
